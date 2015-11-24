ab86302cc6f3cb0c9512da533c3e8d3a6eed2d00
FrederikSorensen <fred5277@easv365.dk>
2015-11-24 13:29:07 +0100

    Oprettet about, contact, team og tournament controller

---
+++ VOLLEYTEAMEsbjergClient/VOLLEYTEAMEsbjergClient/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VOLLEYTEAMEsbjergClient.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Contact")] string contact)
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(id);
+        }
+
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "Id, Contact")] string contact)
+        {
+            return View();
+        }
+    }
+}
---
+++ VOLLEYTEAMEsbjergClient/VOLLEYTEAMEsbjergClient/Controllers/TournamentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VOLLEYTEAMEsbjergClient.Controllers
+{
+    public class TournamentController : Controller
+    {
+        // GET: Tournament
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Tournament")] string tournament)
+        {
+            return View(tournament);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(id);
+        }
+
+        [HttpPost]
+        public ActionResult Edit([Bind(Include = "Id, Tournament")] string tournament)
+        {
+            return View(tournament);
+        }
+    }
+}
