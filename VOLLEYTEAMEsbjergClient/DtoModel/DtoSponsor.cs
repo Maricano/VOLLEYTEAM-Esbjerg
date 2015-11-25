11a67fa8f1c064e913c36bcfd5bfedfbf866406f
Brandtzz <stargateatlantis@hotmail.dk>
2015-11-25 09:26:22 +0100

    DTO model

---
+++ VOLLEYTEAMEsbjergClient/DtoModel/DtoSponsor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoModel
+{
+    class DtoSponsor
+    {
+        public int Id { get; set; }
+
+        public string Sponsor { get; set; }
+    }
+}
