1ea64b76e7930477107fb94832bf25e3709a77ef
Brandtzz <stargateatlantis@hotmail.dk>
2015-11-30 10:46:56 +0100

    News controller

---
+++ VOLLEYTEAMEsbjergClient/VOLLEYTEAMEsbjergClient/Controllers/NewsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VOLLEYTEAMEsbjergClient.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "News")] string news)
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return View(id);
+        }
+
+        
+
+        
+
+    }
+}
