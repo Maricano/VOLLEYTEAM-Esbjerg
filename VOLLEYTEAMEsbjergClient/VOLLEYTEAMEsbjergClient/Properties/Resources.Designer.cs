b7779fc8a5ab9356c66545edb600ee6578423394
Brandtzz <stargateatlantis@hotmail.dk>
2015-12-03 09:17:21 +0100

    Logo

--- VOLLEYTEAMEsbjergClient/VOLLEYTEAMEsbjergClient/Properties/Resources.Designer.cs
+++ VOLLEYTEAMEsbjergClient/VOLLEYTEAMEsbjergClient/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace VOLLEYTEAMEsbjergClient.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap LogoSortBaggrund {
+            get {
+                object obj = ResourceManager.GetObject("LogoSortBaggrund", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
