e54d72c6499744918ab6c43e2a9fa9eb5d88915d
Brandtzz <stargateatlantis@hotmail.dk>
2015-12-15 09:57:33 +0100

    Fejl fjernet

---
+++ VOLLEYTEAMEsbjergClient/VOLLEYTEAMEsbjergClient/Models/PlayerTeamIdViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace VOLLEYTEAMEsbjergClient.Models
+{
+    public class PlayerTeamIdViewModel
+    {
+        [Display(Name = "Team Id")]
+        public int SelectedTeamId { get; set; }
+       // public IEnumerable<SelectListItem> TeamName { get; set; }
+    }
+}
